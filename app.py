0a6c62191f9938f0c58aa4b49fbaaae74cd8ca40
Raghuvar Tandon <raghuvartandon15@gmail.com>
2024-10-03 19:07:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+        response = requests_retry_session().get(url)
+        data = response.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+        return None
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching poster: {e}")
+        return None
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+
+
+movies_dict=pickle.load(open('movie_dict.pkl', 'rb'))
+movies=pd.DataFrame(movies_dict)
+
+similarity=pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox('hi', movies['title'].values)
+
+if st.button('Recommend'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
